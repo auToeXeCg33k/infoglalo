bce6d2a9dfd62bac72b2044116905117d7cb2375
leopph69 <levente.loffler99@gmail.com>
2021-04-18 13:38:25 +0200

    Removed method first param name check and PEP8 coding style inspections.

--- py/src/app.py
+++ py/src/app.py
@@ -4,5 +4,5 @@ import tkinter
 class App(tkinter.Tk):
     def __init__(this, *args, **kwargs):
         tkinter.Tk.__init__(this, *args, **kwargs)
-        this.title()
+        this.title("App")
         print("Hello, World!")
