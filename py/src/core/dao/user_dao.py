b5ab61a473f116e0a7e8ead3e96fd38f0ae33093
leopph69 <levente.loffler99@gmail.com>
2021-04-27 10:58:49 +0200

    Added birth date implementation reminder. Changed user dao to reflect db changes.

--- py/src/core/dao/user_dao.py
+++ py/src/core/dao/user_dao.py
@@ -6,11 +6,11 @@ from typing import Union
 
 
 class UserDAO:
-    def insert(self, uname, email, pwdhash, salt) -> bool:
+    def insert(self, uname: str, email: str, pwdhash: str, salt: str, birth_date: cx_Oracle.Date) -> bool:
         try:
             with cx_Oracle.connect(ConfigLoader.get_db_user(), ConfigLoader.get_db_pwd(), ConfigLoader.get_db_url(), encoding=ConfigLoader.get_db_encoding()) as connection:
                 cursor = connection.cursor()
-                cursor.execute("INSERT INTO JATEKOS(FELHASZNALONEV, EMAIL, ADMIN, JELSZO, PONTSZAM, SALT) VALUES (:1, :2, :3, :4, :5, :6)", [uname, email, False, pwdhash, 0, salt])
+                cursor.execute("INSERT INTO JATEKOS(FELHASZNALONEV, EMAIL, ADMIN, JELSZO, SALT, SZULDATUM) VALUES (:1, :2, :3, :4, :5, :6)", [uname, email, False, pwdhash, salt, birth_date])
                 return True
 
         except Exception as e:
@@ -18,11 +18,11 @@ class UserDAO:
             return False
 
 
-    def get(self, uname) -> Union[tuple[str, str,str, str, bool, int], None]:
+    def get(self, uname: str) -> Union[tuple[str, str, str, str, bool, cx_Oracle.Date, int, int, int], None]:
         try:
             with cx_Oracle.connect(ConfigLoader.get_db_user(), ConfigLoader.get_db_pwd(), ConfigLoader.get_db_url(), encoding=ConfigLoader.get_db_encoding()) as connection:
                 cursor = connection.cursor()
-                cursor.execute("SELECT FELHASZNALONEV, EMAIL, JELSZO, SALT, ADMIN, PONTSZAM  FROM JATEKOS WHERE FELHASZNALONEV = :1", [uname])
+                cursor.execute("SELECT FELHASZNALONEV, EMAIL, JELSZO, SALT, ADMIN, SZULDATUM, KONNYUPONT, KOZEPESPONT, NEHEZPONT  FROM JATEKOS WHERE FELHASZNALONEV = :1", [uname])
                 result: list[tuple] = cursor.fetchall()
 
                 if len(result) == 0:
