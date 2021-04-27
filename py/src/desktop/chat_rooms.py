f17f989b575d3012dbfd7e1803e26ed49f5f1300
Szász Attila <h985542@stud.u-szeged.hu>
2021-04-27 12:43:31 +0200

    social dao

--- py/src/desktop/chat_rooms.py
+++ py/src/desktop/chat_rooms.py
@@ -1,36 +1,36 @@
 import tkinter as tk
 
 from .scrollable_window import ScrollableWindow
-
+from ..core.dao.social_dao import SocialDAO
 
 class ChatRoom(ScrollableWindow):
     def __init__(this, data) -> None:
         ScrollableWindow.__init__(this, data)
 
-        this.reset()
+        #DAO
+        socialDAO = SocialDAO()
 
-
-    #TODO: Rename labels
-    def reset(this) -> None:
         #TITLE
         tk.Label(this, text="Közösségi szobák", font=(None, 25)).grid(row=0, column=0, sticky="NESW")
-
         #FRAME IN THE CANVAS (MAIN FRAME)
         this.main_frame.rowconfigure(index=0, weight=1)
         this.main_frame.columnconfigure(index=0, weight=1)
         this.main_frame.columnconfigure(index=1, weight=3)
 
         #FRAME FOR ROOM NAME
-        room_frame = tk.LabelFrame(this.main_frame, text="Szobak", font=(None, 15))
+        room_frame = tk.LabelFrame(this.main_frame, text="Szobák", font=(None, 15))
         room_frame.grid(row=0, column=0,sticky="NESW")
 
-        for i in range(10):
+        #ADD ROOMS FROM DB
+        rooms:list[tuple[int, str]] = socialDAO.get_room()
+
+        for i in range(len(rooms)):
             room_frame.rowconfigure(index=i, weight=1)
-            name = "Szoba " + str(i)
-            tk.Button(room_frame, command=this.to_room(id), text=name).grid(row=i, column=0, sticky="NESW")
+            tk.Button(room_frame, command=this.to_room(rooms[i][0]), text=rooms[i][1]).grid(row=i, column=0, sticky="NESW")
+        
 
         #FRAME FOR MAIN MESSAGES
-        main_message_frame = tk.LabelFrame(this.main_frame, text="Uzenetek",font=(None, 15))
+        main_message_frame = tk.LabelFrame(this.main_frame, text="Üzenetek",font=(None, 15))
         main_message_frame.grid(row=0, column=1,sticky="NESW")
         main_message_frame.rowconfigure(index=0, weight=1)
         main_message_frame.rowconfigure(index=1, weight=1)
@@ -40,6 +40,8 @@ class ChatRoom(ScrollableWindow):
         messages_frame = tk.Frame(main_message_frame)
         messages_frame.grid(row=0, column=0, sticky="NESW")
 
+
+        #ADD MESSAGES FROM DB
         for i in range(15):
             text = "User" + str(i) + ": Ez egy uzenet."
             tk.Label(messages_frame, text=text, font=(None, 10)).grid(row=i, sticky="W")
@@ -58,6 +60,12 @@ class ChatRoom(ScrollableWindow):
         send_button.grid(row=1, column=0, sticky="NESW")
 
         writer_frame.grid(row=1, column=0, sticky="NESW")
+
+        this.reset()
+
+
+    def reset(this) -> None:
+        pass
 
 
 
