8464eb8f7f58c575fbf0c983e7cd2e25ea5a331c
leopph69 <levente.loffler99@gmail.com>
2021-04-28 12:05:22 +0200

    Fixed a bug DuelDAO SQLs.

--- py/src/core/dao/duel_dao.py
+++ py/src/core/dao/duel_dao.py
@@ -23,12 +23,12 @@ class DuelDAO:
 
     def get_incoming(this, uname: str) -> list[str]:
         return this.executesql("SELECT PARBAJRAHIV.JATEKOS FROM PARBAJRAHIV \
-                               INNER JOIN PARBAJRAHIVOTT ON PARBAJRAHIV.JATEKOS = PARBAJRAHIVOTT.JATEKOS \
+                               INNER JOIN PARBAJRAHIVOTT ON PARBAJRAHIV.ID = PARBAJRAHIVOTT.ID \
                                INNER JOIN PARBAJ ON PARBAJ.ID = PARBAJRAHIV.ID \
                                WHERE PENDING = 1 AND PARBAJRAHIVOTT.JATEKOS = :1", [uname])
 
     def get_requested(this, uname: str) -> list[str]:
         return this.executesql("SELECT PARBAJRAHIVOTT.JATEKOS FROM PARBAJRAHIV \
-                               INNER JOIN PARBAJRAHIVOTT ON PARBAJRAHIV.JATEKOS = PARBAJRAHIVOTT.JATEKOS \
+                               INNER JOIN PARBAJRAHIVOTT ON PARBAJRAHIV.ID = PARBAJRAHIVOTT.ID \
                                INNER JOIN PARBAJ ON PARBAJ.ID = PARBAJRAHIV.ID \
                                WHERE PENDING = 1 AND PARBAJRAHIV.JATEKOS = :1", [uname])
