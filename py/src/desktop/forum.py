3de13366a3835bdec73bfe889a80db2f0a0b8303
Szász Attila <h985542@stud.u-szeged.hu>
2021-04-28 13:28:39 +0200

    Back buttons for social frames

--- py/src/desktop/forum.py
+++ py/src/desktop/forum.py
@@ -16,12 +16,12 @@ class Forum(ScrollableWindow):
         tk.Label(this, text="Fórum", font=(None, 25)).grid(row=0, column=0, sticky="NESW")
         #FRAME IN THE CANVAS (MAIN FRAME)
         this.main_frame.rowconfigure(index=0, weight=1)
+        this.main_frame.rowconfigure(index=1, weight=1)
         this.main_frame.columnconfigure(index=0, weight=1)
-        this.main_frame.columnconfigure(index=1, weight=3)
 
         #FRAME FOR MAIN MESSAGES
         main_message_frame = tk.LabelFrame(this.main_frame, text="Üzenetek",font=(None, 15))
-        main_message_frame.grid(row=0, column=1,sticky="NESW")
+        main_message_frame.grid(row=0, column=0,sticky="NESW")
         main_message_frame.rowconfigure(index=0, weight=1)
         main_message_frame.rowconfigure(index=1, weight=1)
         main_message_frame.columnconfigure(index=0, weight=1)
@@ -51,8 +51,15 @@ class Forum(ScrollableWindow):
 
         writer_frame.grid(row=1, column=0, sticky="NESW")
 
+        # BACK BUTTON
+        back_button = tk.Button(this.main_frame, text='Vissza', command=this.go_back)
+        back_button.grid(row=1, column=0, sticky="W")
+
     def reset(this) -> None:
         pass
 
     def send_message(this, content):
         pass
+
+    def go_back(this):
+        this.master.raise_previous_window()
