0f9359bc919ff94572d452f846f54c36854a424a
Süli Tamara <h985669@stud.u-szeged.hu>
2021-05-04 21:13:38 +0200

    ads gui rework + new_ads_window

--- py/src/desktop/ads_window.py
+++ py/src/desktop/ads_window.py
@@ -1,15 +1,26 @@
 import tkinter as tk
-import tkinter.messagebox
+from tkinter import ttk
+
+from .new_ads_window import NewAdsWindow
 from .scrollable_window import ScrollableWindow
 from ..core.dao.ad_dao import AdDAO
+from ..core.config import ConfigLoader
 
 
 class AdsWindow(ScrollableWindow):
     def __init__(this, data) -> None:
         ScrollableWindow.__init__(this, data)
+
+        this.font_family = ConfigLoader.get("font-family")
+        this.font_color = ConfigLoader.get("font-color")
+
+        this.main_frame.columnconfigure(index=0, weight=1)
+        this.main_frame.columnconfigure(index=1, weight=1)
+
+        tk.Label(this, fg=this.font_color, text="Hirdetések", bg=this['bg'], font=(this.font_family, 25)).grid(row=0, column=0, sticky="NESW")
+
         this.reset()
-        tk.Label(this, text="Hirdetések", font=(None, 25)).grid(row=0, column=0, sticky="NESW")
-        
+
     def reset(this) -> None:
         for child in this.main_frame.winfo_children():
             child.destroy()
@@ -19,11 +30,14 @@ class AdsWindow(ScrollableWindow):
         i = 0
         for i in range (len(data)):
             this.main_frame.rowconfigure(index=i, weight=1)
-            img_button = tk.Button(this.main_frame, image=data[i][2], command=lambda index=i: this.get_info(data, index))
+            img_button = ttk.Button(this.main_frame, image=data[i][2], command=lambda index=i: this.get_info(data, index))
             img_button.image = data[i][2]
-            img_button.grid(row=i, column=0, sticky="NEWS")
+            img_button.grid(row=i, column=0, columnspan=2)
         this.main_frame.rowconfigure(index=i+1, weight=1)
-        tk.Button(this.main_frame, text="Vissza", command=this.go_back).grid(row=i+1, column=0, sticky="NEWS")
+        tk.Button(this.main_frame, text="Vissza", command=this.go_back).grid(row=i+1, column=0, sticky="NESW")
+
+        if this.data["user"][4]: # ha admin
+            ttk.Button(this.main_frame, text="Hirdetés felvitele", command=this.go_admin).grid(row=i+1, column=1, sticky="NESW")
 
 
     def get_info(this, data, i) -> None:
@@ -31,10 +45,13 @@ class AdsWindow(ScrollableWindow):
         info_frame.columnconfigure(index=0, weight=1)
         info_frame.rowconfigure(index=0, weight=1)
         info_frame.rowconfigure(index=1, weight=0)
-        tk.Label(info_frame, text=data[i][0], font=(None, 25)).grid(row=0, column=0, sticky="NESW")
-        tk.Label(info_frame, text=data[i][1], font=(None, 15)).grid(row=1, column=0, sticky="NESW")
+        tk.Label(info_frame, fg=this.font_color, text=data[i][0], bg=this['bg'], font=(this.font_family, 25)).grid(row=0, column=0, sticky="NESW")
+        tk.Label(info_frame, fg=this.font_color, text=data[i][1], bg=this['bg'], font=(this.font_family, 15)).grid(row=1, column=0, sticky="NESW")
 
     def go_back(this) -> None:
         this.master.raise_previous_window()
+
+    def go_admin(this) -> None:
+        this.master.raise_window(NewAdsWindow)
 
 
---
+++ py/src/desktop/new_ads_window.py
@@ -0,0 +1,62 @@
+import tkinter
+from tkinter import filedialog
+from tkinter import ttk
+import os
+
+from .window import Window
+from ..core.config import ConfigLoader
+
+class NewAdsWindow(Window):
+    def __init__(this, data):
+        Window.__init__(this, data)
+
+        this.rowconfigure(index=0, weight=50)
+        this.rowconfigure(index=1, weight=1)
+        this.rowconfigure(index=2, weight=1)
+        this.rowconfigure(index=3, weight=1)
+        this.rowconfigure(index=4, weight=1)
+        this.rowconfigure(index=5, weight=1)
+        this.rowconfigure(index=6, weight=70)
+        this.columnconfigure(index=0, weight=1)
+        this.columnconfigure(index=1, weight=1)
+
+        this.font_family = ConfigLoader.get("font-family")
+        this.font_color = ConfigLoader.get("font-color")
+
+        tkinter.Label(this, fg=this.font_color, bg=this['bg'], text="Hirdetés felvitele", font=(this.font_family, 25)).grid(row=0, column=0, columnspan=2)
+        tkinter.Label(this, fg=this.font_color, bg=this['bg'], text="Cím", font=(this.font_family, 15)).grid(row=1, column=0, sticky="E")
+        tkinter.Label(this, fg=this.font_color, bg=this['bg'], text="Szöveg", font=(this.font_family, 15)).grid(row=2, column=0, sticky="E")
+        tkinter.Label(this, fg=this.font_color, bg=this['bg'], text="Plakát", font=(this.font_family, 15)).grid(row=3, column=0, sticky="E")
+        this.filename = ""
+        this.filename_label = tkinter.Label(this, fg=this.font_color, bg=this['bg'], text="Nincs feltöltött file", font=(this.font_family, 10))
+        this.filename_label.grid(row=4, column=1, sticky="NW")
+
+        ttk.Button(this, command=this.upload_poster, text="Feltöltés").grid(row=3, column=1, sticky="W")
+        ttk.Button(this, command=this.go_back, text="Vissza").grid(row=6, column=0, sticky="E")
+        ttk.Button(this, command=this.insert_ad, text="Felvitel").grid(row=6, column=1, sticky="W")
+
+        this.title = tkinter.Entry(this)
+        this.text = tkinter.Entry(this)
+
+        this.title.grid(row=1, column=1, sticky="W")
+        this.text.grid(row=2, column=1, sticky='W')
+
+
+        this.reset()
+
+    def reset(this) -> None:
+        this.filename = ""
+        this.filename_label["text"] = "Nincs feltöltött file"
+        this.title.delete(0, 'end')
+        this.text.delete(0, 'end')
+
+    def upload_poster(this) -> None:
+        this.filename = filedialog.askopenfilename()
+        if this.filename != "" :
+            this.filename_label["text"] = os.path.basename(this.filename)
+
+    def go_back(this) -> None:
+        this.master.raise_previous_window()
+
+    def insert_ad(this) -> None:
+        pass
