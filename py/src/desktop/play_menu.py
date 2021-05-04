dd5b851c2ae262b796f7768808b32325afd20028
leopph69 <levente.loffler99@gmail.com>
2021-05-04 20:28:38 +0200

    Add font color entry to config

--- py/src/desktop/play_menu.py
+++ py/src/desktop/play_menu.py
@@ -1,35 +1,41 @@
 import tkinter
 
+from tkinter.ttk import Button
+
 from .window import Window
 from .duel_menu import DuelMenuWindow
 from ..core.config import ConfigLoader
+
 
 class PlayMenuWindow(Window):
     def __init__(this, data):
         Window.__init__(this, data)
 
-        this.rowconfigure(index=0, weight=1)
+        this.rowconfigure(index=0, weight=50)
         this.rowconfigure(index=1, weight=1)
         this.rowconfigure(index=2, weight=1)
         this.rowconfigure(index=3, weight=1)
         this.rowconfigure(index=4, weight=1)
-        this.rowconfigure(index=5, weight=1)
+        this.rowconfigure(index=5, weight=20)
+        this.rowconfigure(index=6, weight=1)
+        this.rowconfigure(index=7, weight=100)
         this.columnconfigure(index=0, weight=1)
 
         this.reset()
 
     def reset(this) -> None:
-
+        font_family = ConfigLoader.get("font-family")
+        font_color = ConfigLoader.get("font-color")
 
         #MENU TITLE
-        tkinter.Label(this, text="Válassz játékmódot!", font=(None, 25)).grid(row=0, column=0, sticky="NESW")
+        tkinter.Label(this, text="Válassz játékmódot!", fg=font_color, bg=this["bg"], font=(font_family, 26)).grid(row=0, column=0, sticky="NESW")
 
         #MENU BUTTONS
-        tkinter.Button(this, command=this.go_to_iq, text="IQ teszt").grid(row=1, column=0, sticky="NESW")
-        tkinter.Button(this, command=this.go_to_comp, text="Verseny").grid(row=2, column=0, sticky="NESW")
-        tkinter.Button(this, command=this.go_to_duel, text="Párbaj").grid(row=3, column=0, sticky="NESW")
-        tkinter.Button(this, command=this.go_to_test, text="Tematikus teszt").grid(row=4, column=0, sticky="NESW")
-        tkinter.Button(this, command=this.go_back, text="Vissza").grid(row=5, column=0, sticky="NESW")
+        Button(this, command=this.go_to_iq, text="IQ teszt").grid(row=1, column=0)
+        Button(this, command=this.go_to_comp, text="Verseny").grid(row=2, column=0)
+        Button(this, command=this.go_to_duel, text="Párbaj").grid(row=3, column=0)
+        Button(this, command=this.go_to_test, text="Tematikus teszt").grid(row=4, column=0)
+        Button(this, command=this.go_back, text="Vissza").grid(row=6, column=0)
 
     def go_to_iq(this) -> None:
         pass
