a0b6ca450cd459219b382ad139031b4acd9c6d15
leopph69 <levente.loffler99@gmail.com>
2021-05-04 20:17:52 +0200

    Improve main menu visuals

--- py/src/desktop/main_menu.py
+++ py/src/desktop/main_menu.py
@@ -1,37 +1,45 @@
 import tkinter
 
+from tkinter.ttk import Button
+
 from .social_menu import SocialMenu
 from .window import Window
 from .ads_window import AdsWindow
 from .toplist import ToplistWindow
 from .play_menu import PlayMenuWindow
+from ..core.config import ConfigLoader
 
 class MainMenuWindow(Window):
     def __init__(this, data) -> None:
         Window.__init__(this, data)
 
-        this.rowconfigure(index=0, weight=2)
+        this.rowconfigure(index=0, weight=50)
         this.rowconfigure(index=1, weight=1)
         this.rowconfigure(index=2, weight=1)
         this.rowconfigure(index=3, weight=1)
         this.rowconfigure(index=4, weight=1)
         this.rowconfigure(index=5, weight=1)
-        this.rowconfigure(index=6, weight=1)
+        this.rowconfigure(index=6, weight=20)
+        this.rowconfigure(index=7, weight=1)
+        this.rowconfigure(index=8, weight=100)
         this.columnconfigure(index=0, weight=1)
 
         this.reset()
 
     def reset(this) -> None:
+        font_family = ConfigLoader.get("font")
+        font_color = "snow"
+
         #MENU TITLE
-        tkinter.Label(this, text="Menü", font=(None, 25)).grid(row=0, column=0, sticky="NESW")
+        tkinter.Label(this, text="Üdv, " + this.data["user"][0] + "!", fg=font_color, bg=this["bg"], font=(font_family, 26)).grid(row=0, column=0)
 
         #MENU BUTTONS
-        tkinter.Button(this, command=this.go_to_play, text="Játék").grid(row=1, column=0, sticky="NESW")
-        tkinter.Button(this, command=this.go_to_ranking, text="Rangsor").grid(row=2, column=0, sticky="NESW")
-        tkinter.Button(this, command=this.go_to_social, text="Közösségi").grid(row=3, column=0, sticky="NESW")
-        tkinter.Button(this, command=this.go_to_ad, text="Hirdetések").grid(row=4, column=0, sticky="NESW")
-        tkinter.Button(this, command=this.go_to_settings, text="Beállítások").grid(row=5, column=0, sticky="NESW")
-        tkinter.Button(this, command=this.logout, text="Kijelentkezés").grid(row=6, column=0, sticky="NESW")
+        Button(this, command=this.go_to_play, text="Játék").grid(row=1, column=0)
+        Button(this, command=this.go_to_ranking, text="Rangsor").grid(row=2, column=0)
+        Button(this, command=this.go_to_social, text="Közösségi").grid(row=3, column=0)
+        Button(this, command=this.go_to_ad, text="Hirdetések").grid(row=4, column=0)
+        Button(this, command=this.go_to_settings, text="Beállítások").grid(row=5, column=0)
+        Button(this, command=this.logout, text="Kijelentkezés").grid(row=7, column=0)
 
     def go_to_play(this) -> None:
         this.master.raise_window(PlayMenuWindow)
