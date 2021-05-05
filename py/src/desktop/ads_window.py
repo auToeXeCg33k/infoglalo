3822256bf3cdf0febdca2f92291b787b43c51278
Süli Tamara <h985669@stud.u-szeged.hu>
2021-05-05 18:37:48 +0200

    delete ad, ad window fix

--- py/src/desktop/ads_window.py
+++ py/src/desktop/ads_window.py
@@ -1,5 +1,5 @@
 import tkinter
-import tkinter as tk
+import tkinter
 from tkinter import messagebox
 from tkinter import ttk
 
@@ -21,43 +21,38 @@ class AdsWindow(ScrollableWindow):
         this.main_frame.columnconfigure(index=0, weight=1)
         this.main_frame.columnconfigure(index=1, weight=1)
 
-        tk.Label(this, fg=this.font_color, text="Hirdetések", bg=this['bg'], font=(this.font_family, 25)).grid(row=0, column=0, columnspan=2, sticky="NESW")
+        tkinter.Label(this, fg=this.font_color, text="Hirdetések", bg=this['bg'], font=(this.font_family, 25)).grid(row=0, column=0, columnspan=2, sticky="NESW")
 
         this.dao = AdDAO()
 
         this.reset()
 
-    # data[i][2] -> img, height = 300px
     def reset(this) -> None:
         for child in this.main_frame.winfo_children():
             child.destroy()
 
         ttk.Button(this.main_frame, text="Vissza", command=this.go_back).place(x=10, y=10)
         if this.data["user"][4]:  # ha admin
-             ttk.Button(this.main_frame, text="Hirdetés felvitele", command=this.go_admin).place(x=700, y=10)
+             ttk.Button(this.main_frame, text="Hirdetés felvitele", command=this.go_admin).place(x=800, y=10)
 
         dao = AdDAO()
         data = dao.find_all()
-        print(len(data))
         for i in range (len(data)):
             img_button = ttk.Button(this.main_frame, image=data[i][2], command=lambda index=i: this.get_info(data, index))
             img_button.image = data[i][2]
-            img_button.place(x=110, y=((len(data)-1) * 340) - i * 330)
-            ttk.Button(this.main_frame, text="X", command=lambda index=i: this.delete_ad(index + 1)).place(x=110 + data[i][2].width() + 10, y=((len(data) - 1) * 340) - i * 330)
-
-
-
+            img_button.place(x=110, y=((len(data)-1) * 330) + 10 - i * 330)
+            ttk.Button(this.main_frame, text="X", command=lambda index=i: this.delete_ad(index)).place(x=110 + data[i][2].width() + 10, y=((len(data)-1) * 330) + 10 - i * 330)
 
     def get_info(this, data, i) -> None:
-        info_frame = tk.Toplevel()
+        info_frame = tkinter.Toplevel()
         info_frame.columnconfigure(index=0, weight=1)
         info_frame.rowconfigure(index=0, weight=1)
         info_frame.rowconfigure(index=1, weight=0)
-        tk.Label(info_frame, fg=this.font_color, text=data[i][0], bg=this['bg'], font=(this.font_family, 25)).grid(row=0, column=0, sticky="NESW")
-        tk.Label(info_frame, fg=this.font_color, text=data[i][1], bg=this['bg'], font=(this.font_family, 15)).grid(row=1, column=0, sticky="NESW")
+        tkinter.Label(info_frame, fg=this.font_color, text=data[i][0], bg=this['bg'], font=(this.font_family, 25)).grid(row=0, column=0, sticky="NESW")
+        tkinter.Label(info_frame, fg=this.font_color, text=data[i][1], bg=this['bg'], font=(this.font_family, 15)).grid(row=1, column=0, sticky="NESW")
 
     def delete_ad(this, index) -> None:
-        msgbox = tk.messagebox.askquestion('Hirdetés törlése', 'Biztosan ki szeretnéd törölni a hirdetést?',
+        msgbox = tkinter.messagebox.askquestion('Hirdetés törlése', 'Biztosan ki szeretnéd törölni a hirdetést?',
                                            icon='warning')
 
         if msgbox == 'yes':
