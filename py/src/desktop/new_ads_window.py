cea1dd95d33a5837993ffe8f63a50d9e68d08317
Süli Tamara <h985669@stud.u-szeged.hu>
2021-05-05 19:21:46 +0200

    new_ad_window gui rework

--- py/src/desktop/new_ads_window.py
+++ py/src/desktop/new_ads_window.py
@@ -13,37 +13,32 @@ class NewAdsWindow(Window):
     def __init__(this, data):
         Window.__init__(this, data)
 
-        this.rowconfigure(index=0, weight=50)
+        this.rowconfigure(index=0, weight=1)
         this.rowconfigure(index=1, weight=1)
         this.rowconfigure(index=2, weight=1)
-        this.rowconfigure(index=3, weight=1)
+        this.rowconfigure(index=3, weight=300)
         this.rowconfigure(index=4, weight=1)
-        this.rowconfigure(index=5, weight=1)
-        this.rowconfigure(index=6, weight=70)
-        this.columnconfigure(index=0, weight=1)
-        this.columnconfigure(index=1, weight=1)
+        this.columnconfigure(index=0, weight=70)
+        this.columnconfigure(index=1, weight=30)
 
         this.font_family = ConfigLoader.get("font-family")
         this.font_color = ConfigLoader.get("font-color")
 
         tkinter.Label(this, fg=this.font_color, bg=this['bg'], text="Hirdetés felvitele", font=(this.font_family, 25)).grid(row=0, column=0, columnspan=2)
-        tkinter.Label(this, fg=this.font_color, bg=this['bg'], text="Cím", font=(this.font_family, 15)).grid(row=1, column=0, sticky="E")
-        tkinter.Label(this, fg=this.font_color, bg=this['bg'], text="Szöveg", font=(this.font_family, 15)).grid(row=2, column=0, sticky="E")
-        tkinter.Label(this, fg=this.font_color, bg=this['bg'], text="Plakát", font=(this.font_family, 15)).grid(row=3, column=0, sticky="E")
         this.img = None
         this.filename = ""
         this.filename_label = tkinter.Label(this, fg=this.font_color, bg=this['bg'], text="Nincs feltöltött file", font=(this.font_family, 10))
-        this.filename_label.grid(row=4, column=1, sticky="NW")
+        this.filename_label.grid(row=2, column=1, sticky="NW")
 
-        ttk.Button(this, command=this.upload_poster, text="Feltöltés").grid(row=3, column=1, sticky="W")
-        ttk.Button(this, command=this.go_back, text="Vissza").grid(row=6, column=0, sticky="E")
-        ttk.Button(this, command=this.insert_ad, text="Felvitel").grid(row=6, column=1, sticky="W")
+        ttk.Button(this, command=this.upload_poster, text="Feltöltés").grid(row=1, column=1)
+        ttk.Button(this, command=this.go_back, text="Vissza").grid(row=4, column=0, sticky="E")
+        ttk.Button(this, command=this.insert_ad, text="Felvitel").grid(row=4, column=1, sticky="W")
 
         this.title = tkinter.Entry(this)
         this.text = tkinter.Entry(this)
 
-        this.title.grid(row=1, column=1, sticky="W")
-        this.text.grid(row=2, column=1, sticky='W')
+        this.title.grid(row=1, column=0, rowspan=2, sticky="NEWS")
+        this.text.grid(row=3, column=0, columnspan=2, sticky="NEWS")
 
         this.dao = AdDAO()
 
@@ -55,6 +50,8 @@ class NewAdsWindow(Window):
         this.filename_label["text"] = "Nincs feltöltött file"
         this.title.delete(0, 'end')
         this.text.delete(0, 'end')
+        this.title.insert(0, 'Cím...')
+        this.text.insert(0, 'Szöveg...')
 
     def upload_poster(this) -> None:
         this.filename = filedialog.askopenfilename()
