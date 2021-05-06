9d5279e06f7d60edac16c44b9e46668736802334
leopph69 <levente.loffler99@gmail.com>
2021-05-06 14:36:06 +0200

    Basic duel gameplay works

--- py/src/desktop/duel_game_window.py
+++ py/src/desktop/duel_game_window.py
@@ -1,4 +1,5 @@
 import tkinter
+import tkinter.ttk
 
 from .window import Window
 from ..core.config import ConfigLoader
@@ -17,19 +18,17 @@ class DuelGameWindow(Window):
         tkinter.Label(master=this, fg=font_color, bg=this["bg"], font=(font_family, 26), text="Párbaj").pack(side=tkinter.TOP, fill=tkinter.X)
 
         this.question_label = tkinter.Label(master=this, fg=font_color, bg=this["bg"], font=(font_family, 20))
-        this.question_label.pack(side=tkinter.TOP, fill=tkinter.X, pady=20)
 
-        answers_frame = tkinter.Frame(master=this, bg=this["bg"])
-        answers_frame.pack(side=tkinter.TOP, pady=20)
+        this.answers_frame = tkinter.Frame(master=this, bg=this["bg"])
 
         for i in range(2):
-            answers_frame.rowconfigure(index=i, weight=1)
-            answers_frame.columnconfigure(index=i, weight=1)
+            this.answers_frame.rowconfigure(index=i, weight=1)
+            this.answers_frame.columnconfigure(index=i, weight=1)
 
-        this.ans_a_label = tkinter.Label(master=answers_frame, fg=font_color, bg=this["bg"], font=(font_family, 16))
-        this.ans_b_label = tkinter.Label(master=answers_frame, fg=font_color, bg=this["bg"], font=(font_family, 16))
-        this.ans_c_label = tkinter.Label(master=answers_frame, fg=font_color, bg=this["bg"], font=(font_family, 16))
-        this.ans_d_label = tkinter.Label(master=answers_frame, fg=font_color, bg=this["bg"], font=(font_family, 16))
+        this.ans_a_label = tkinter.Label(master=this.answers_frame, fg=font_color, bg=this["bg"], font=(font_family, 16))
+        this.ans_b_label = tkinter.Label(master=this.answers_frame, fg=font_color, bg=this["bg"], font=(font_family, 16))
+        this.ans_c_label = tkinter.Label(master=this.answers_frame, fg=font_color, bg=this["bg"], font=(font_family, 16))
+        this.ans_d_label = tkinter.Label(master=this.answers_frame, fg=font_color, bg=this["bg"], font=(font_family, 16))
 
         this.ans_a_label.grid(row=0, column=0, padx=(0, 30))
         this.ans_b_label.grid(row=0, column=1, padx=(30, 0))
@@ -41,8 +40,20 @@ class DuelGameWindow(Window):
         this.ans_c_label.bind("<Button-1>", lambda e: this.on_selected("c"))
         this.ans_d_label.bind("<Button-1>", lambda e: this.on_selected("d"))
 
+        this.result_frame = tkinter.Frame(master=this, bg=this["bg"])
+        this.correctness_label = tkinter.Label(master=this.result_frame, fg=font_color, bg=this["bg"], font=(font_family, 20))
+        this.result_label = tkinter.Label(master=this.result_frame, fg=font_color, bg=this["bg"], font=(font_family, 14))
+        tkinter.ttk.Button(master=this.result_frame, text="Vissza", command=this.go_back).pack(side=tkinter.BOTTOM)
+
+        this.correctness_label.pack()
+        this.result_label.pack()
+
 
     def reset(this) -> None:
+        this.result_frame.pack_forget()
+        this.question_label.pack(side=tkinter.TOP, fill=tkinter.X, pady=20)
+        this.answers_frame.pack(side=tkinter.TOP, pady=20)
+
         this.question_label["text"] = this.data["duel"]["question"]
         this.ans_a_label["text"] = this.data["duel"]["answers"][0]
         this.ans_b_label["text"] = this.data["duel"]["answers"][1]
@@ -53,8 +64,30 @@ class DuelGameWindow(Window):
     def on_selected(this, letter: str) -> None:
         this.dao.add_answer(this.data["duel"]["id"], this.data["user"][0], letter.upper())
 
-        if this.data["duel"]["correct_ans"].lower() == letter:
-            print("correct answer")
+        this.question_label.pack_forget()
+        this.answers_frame.pack_forget()
 
+        this_is_correct = this.data["duel"]["correct_ans"].lower() == letter
+        this.correctness_label["text"] = "A válaszod helyes volt!" if this_is_correct else "A válaszod helytelen volt."
+
+        other_player = this.data["duel"]["challenger"] if this.data["duel"]["challenger"] == this.data["user"][0] else this.data["duel"]["challenged"]
+
+        if this.dao.has_both_answers(this.data["duel"]["id"]):
+            other_is_correct =  this.dao.get_answer(this.data["duel"]["id"], other_player).lower() ==this.data["duel"]["correct_ans"].lower()
+
+            if other_is_correct and this_is_correct:
+                this.result_label["text"] = "Mindketten helyes választ adtatok. Az eredmény döntetlen."
+            elif not other_is_correct and this_is_correct:
+                this.result_label["text"] = "Győztél!"
+            elif other_is_correct and not this_is_correct:
+                this.result_label["text"] = "Vesztettél..."
+            else:
+                this.result_label["text"] = "Mindketten helytelen választ adtatok. Az eredmény döntetlen."
         else:
-            print("bad answer")
+            this.result_label["text"] = other_player + " még nem adott választ."
+
+        this.result_frame.pack(side=tkinter.TOP, pady=50)
+
+
+    def go_back(this) -> None:
+        this.master.raise_previous_window()
