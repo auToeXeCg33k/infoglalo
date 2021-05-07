c1e1e065d5f56cae10f1fc1f5e65fcf85eadae17
leopph69 <levente.loffler99@gmail.com>
2021-05-07 17:30:20 +0200

    Improve test visuals

--- py/src/desktop/test_game_window.py
+++ py/src/desktop/test_game_window.py
@@ -12,6 +12,7 @@ class TestGameWindow(Window):
 
         font_color = ConfigLoader.get("font-color")
         font_family = ConfigLoader.get("font-family")
+        this.accent_color = ConfigLoader.get("accent-color")
 
 
         this.dao = TestDAO()
@@ -35,25 +36,29 @@ class TestGameWindow(Window):
         # INITIAL SETUP SCREEN
         this.setup_frame = tkinter.Frame(this, bg=this["bg"])
 
+        tkinter.Label(master=this.setup_frame, bg=this["bg"], fg=font_color, font=(font_family, 26), text="Válassz témakört és nehézséget!").pack(pady=(50, 0))
+
         this.current_category = tkinter.StringVar(this.setup_frame)
         this.category_menu = tkinter.ttk.Combobox(this.setup_frame, state="readonly", textvariable=this.current_category)
-        this.category_menu.pack()
+        this.category_menu.pack(pady=(25, 0))
 
         this.current_difficulty = tkinter.StringVar(this.setup_frame)
         this.difficulty_menu = tkinter.ttk.Combobox(this.setup_frame, state="readonly", textvariable=this.current_difficulty, values=list(this.data["test"]["difficulties"]))
         this.difficulty_menu.pack()
 
-        tkinter.ttk.Button(master=this.setup_frame, command=this.on_options_selected, text="Kezdés").pack()
-        tkinter.ttk.Button(master=this.setup_frame, command=this.go_back, text="Vissza").pack()
+        tkinter.ttk.Button(master=this.setup_frame, command=this.on_options_selected, text="Kezdés").pack(pady=(25, 0))
+        tkinter.ttk.Button(master=this.setup_frame, command=this.go_back, text="Vissza").pack(pady=(5, 0))
 
 
         # ACTUAL GAME SCREEN
         this.game_frame = tkinter.Frame(this, bg=this["bg"])
-        this.question_label = tkinter.Label(this.game_frame, font=(font_family, 24), fg=font_color, bg=this["bg"])
+        this.question_no_label = tkinter.Label(this.game_frame, font=(font_family, 26), fg=font_color, bg=this["bg"])
+        this.question_label = tkinter.Label(this.game_frame, font=(font_family, 24, tkinter.UNDERLINE), fg=font_color, bg=this["bg"])
         this.answers_frame = tkinter.Frame(this.game_frame, bg=this["bg"])
 
-        this.question_label.pack()
-        this.answers_frame.pack()
+        this.question_no_label.pack(pady=(50, 0))
+        this.question_label.pack(pady=(15, 0))
+        this.answers_frame.pack(pady=(15, 0))
 
         for i in range(2):
             this.answers_frame.rowconfigure(i, weight=1)
@@ -64,15 +69,25 @@ class TestGameWindow(Window):
         this.ans_c_label = tkinter.Label(this.answers_frame, bg=this["bg"], fg=font_color, font=(font_family, 20))
         this.ans_d_label = tkinter.Label(this.answers_frame, bg=this["bg"], fg=font_color, font=(font_family, 20))
 
-        this.ans_a_label.grid(row=0, column=0)
-        this.ans_b_label.grid(row=0, column=1)
-        this.ans_c_label.grid(row=1, column=0)
-        this.ans_d_label.grid(row=1, column=1)
+        this.ans_a_label.grid(row=0, column=0, padx=(0, 10))
+        this.ans_b_label.grid(row=0, column=1, padx=(10, 0))
+        this.ans_c_label.grid(row=1, column=0, padx=(0, 10), pady=(10, 0))
+        this.ans_d_label.grid(row=1, column=1, padx=(10, 0), pady=(10, 0))
 
         this.ans_a_label.bind("<Button-1>", lambda e: this.on_answer_selected('a'))
         this.ans_b_label.bind("<Button-1>", lambda e: this.on_answer_selected('b'))
         this.ans_c_label.bind("<Button-1>", lambda e: this.on_answer_selected('c'))
         this.ans_d_label.bind("<Button-1>", lambda e: this.on_answer_selected('d'))
+
+        this.ans_a_label.bind("<Enter>", lambda e: this.on_mouse_enter(this.ans_a_label))
+        this.ans_b_label.bind("<Enter>", lambda e: this.on_mouse_enter(this.ans_b_label))
+        this.ans_c_label.bind("<Enter>", lambda e: this.on_mouse_enter(this.ans_c_label))
+        this.ans_d_label.bind("<Enter>", lambda e: this.on_mouse_enter(this.ans_d_label))
+
+        this.ans_a_label.bind("<Leave>", lambda e: this.on_mouse_leave(this.ans_a_label))
+        this.ans_b_label.bind("<Leave>", lambda e: this.on_mouse_leave(this.ans_b_label))
+        this.ans_c_label.bind("<Leave>", lambda e: this.on_mouse_leave(this.ans_c_label))
+        this.ans_d_label.bind("<Leave>", lambda e: this.on_mouse_leave(this.ans_d_label))
 
 
         # RESULTS SCREEN
@@ -133,6 +148,7 @@ class TestGameWindow(Window):
     def on_next_question(this) -> None:
         this.question_index += 1
 
+        this.question_no_label["text"] = str(this.question_index + 1) + ". kérdés"
         this.question_label["text"] = this.data["test"]["tests"][this.question_index]["question"]
         this.ans_a_label["text"] = this.data["test"]["tests"][this.question_index]["answers"][0][0] + ": " + this.data["test"]["tests"][this.question_index]["answers"][0][1]
         this.ans_b_label["text"] = this.data["test"]["tests"][this.question_index]["answers"][1][0] + ": " + this.data["test"]["tests"][this.question_index]["answers"][1][1]
@@ -147,3 +163,9 @@ class TestGameWindow(Window):
 
         for i in range(len(this.answer_correctness_labels)):
             this.answer_correctness_labels[i]["text"] = str(i + 1) + ". " + ("Helyes" if this.given_answers[i] else "Helytelen")
+
+    def on_mouse_enter(this, label: tkinter.Label) -> None:
+        label["bg"]=this.accent_color
+
+    def on_mouse_leave(this, label: tkinter.Label) -> None:
+        label["bg"]=this["bg"]
