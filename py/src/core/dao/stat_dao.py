64363b34f93cbcb2b8dd08bd875be87bc16fe6a9
Süli Tamara <h985669@stud.u-szeged.hu>
2021-05-08 12:04:08 +0200

    statisztika done

--- py/src/core/dao/stat_dao.py
+++ py/src/core/dao/stat_dao.py
@@ -6,14 +6,26 @@ class StatDAO:
         try:
             connection = ConfigLoader.get_connection_pool().acquire()
             cursor = connection.cursor()
-            cursor.execute("SELECT FELHASZNALONEV,ROUND((SYSDATE-JATEKOS.szuldatum)/365) as age FROM JATEKOS")
-            result: list[tuple] = cursor.fetchall()
+            cursor.execute("SELECT ROUND((SYSDATE-JATEKOS.szuldatum)/365) as age FROM JATEKOS")
+            result: list() = cursor.fetchall()
             ConfigLoader.get_connection_pool().release(connection)
 
-            if len(result) == 0:
-                return None
-            else:
-                return result
+            return result
+
+        except Exception as e:
+            print(e)
+            return None
+
+    def get_user_theme(this, user: str):
+        pass
+        try:
+            connection = ConfigLoader.get_connection_pool().acquire()
+            cursor = connection.cursor()
+            cursor.execute("SELECT KERDES.TEMAKOR, COUNT(ADOTTVALASZ.ID) FROM ADOTTVALASZ INNER JOIN kerdes ON adottvalasz.kerdesszoveg = kerdes.szoveg WHERE ADOTTVALASZ.VALASZADO = :1 AND KERDES.BETUJEL = ADOTTVALASZ.VALASZJEL AND kerdes.temakor != 'IQ' GROUP BY KERDES.TEMAKOR", [user])
+            result: list(tuple) = cursor.fetchall()
+            ConfigLoader.get_connection_pool().release(connection)
+
+            return result
 
         except Exception as e:
             print(e)
